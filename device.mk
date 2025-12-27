db5ca6e561ab8fa7754e3a71d400fc500c1f6083
Isaac <isaacvillamante225@gmail.com>
2025-12-27 14:36:59 +0800

    Merge Smiley's TWRP device tree fixes

--- device.mk
+++ device.mk
@@ -6,3 +6,6 @@
 #
 
 LOCAL_PATH := device/samsung/a06
+
+# Copy the files
+PRODUCT_COPY_FILES += $(call find-copy-subdir-files,*,device/samsung/a06/recovery/root,recovery/root)
--- twrp_a06.mk
+++ twrp_a06.mk
@@ -16,7 +16,7 @@ $(call inherit-product, vendor/omni/config/common.mk)
 $(call inherit-product, device/samsung/a06/device.mk)
 
 PRODUCT_DEVICE := a06
-PRODUCT_NAME := omni_a06
+PRODUCT_NAME := twrp_a06
 PRODUCT_BRAND := samsung
 PRODUCT_MODEL := SM-A065F
 PRODUCT_MANUFACTURER := samsung
